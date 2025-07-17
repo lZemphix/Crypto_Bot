431c377bec314771d61f5063bed5eb9c426bfea2
Zemphix <vafinaaa51@gmail.com>
2025-07-17 20:30:33 +0500

    added docstrings

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -1,14 +1,18 @@
 class NoCryptoCurrencyException(Exception):
+    """Raised when a cryptocurrency is not found in your wallet."""
     pass
 
 
 class IncorrectOrdersHistory(Exception):
+    """Raised when the orders history is not in the expected format."""
     pass
 
 
 class IncorrectOpenOrdersList(Exception):
+    """Raised when the open orders list is not in the expected format."""
     pass
 
 
-class OrderPlaceExeption(Exception):
+class OrderPlaceException(Exception):
+    """Raised when an order cannot be placed."""
     pass
