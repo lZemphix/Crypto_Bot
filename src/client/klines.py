662c3ca258551d6da731abe58d5551178227d925
Zemphix <vafinaaa51@gmail.com>
2025-07-19 15:30:04 +0500

    added temp var 'cnt'

--- src/client/klines.py
+++ src/client/klines.py
@@ -1,9 +1,13 @@
+from logging import getLogger
 from client.base import Client
+
+logger = getLogger(__name__)
 
 
 class Klines(Client):
 
     def __init__(self) -> None:
+        self.cnt = 0
         super().__init__()
 
     def get_klines(self, limit: int = 200) -> dict:
@@ -14,6 +18,8 @@ class Klines(Client):
                 limit=limit,
                 category="spot",
             )
+            self.cnt += 1
+            logger.debug(f"Klines req #{self.cnt}")
             return kline["result"]["list"]
         except:
             return None
