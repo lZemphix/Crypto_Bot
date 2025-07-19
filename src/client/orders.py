8e32c622f9d86ec3926492cf28f601fb2e442d64
Zemphix <vafinaaa51@gmail.com>
2025-07-19 15:30:55 +0500

    addded new class

--- src/client/orders.py
+++ src/client/orders.py
@@ -1,3 +1,4 @@
+import json
 from logging import getLogger
 from client.base import Client
 from client.account import account
@@ -5,16 +6,31 @@ from config.config import get_bot_config
 from utils.exceptions import (
     IncorrectOpenOrdersList,
     IncorrectOrdersHistory,
-    OrderPlaceExeption,
+    OrderPlaceException,
 )
+from utils.journal_manager import JournalManager
 
 
 logger = getLogger(__name__)
 
 
-class Orders(Client):
+class Checkup(Client):
     def __init__(self):
         super().__init__()
+
+    def get_accuracy(self) -> int:
+        with open("src/config/bot_config.json", "r") as f:
+            config = json.load(f)
+        symbol = config.get("symbol")
+        info = self.client.get_instruments_info(category="spot", symbol=symbol)
+        min_order_qty = info["result"]["list"][0]["lotSizeFilter"]["minOrderQty"]
+        return len(min_order_qty)
+
+
+class Orders(Checkup):
+    def __init__(self):
+        super().__init__()
+        self.journal = JournalManager()
 
     def get_order_history(self) -> dict:
         """cancelType: [CancelByUser | UNKNOWN]
@@ -58,7 +74,10 @@ class Orders(Client):
         """Places sell order"""
         try:
             coin_name = self.symbol.replace("USDT", "")
-            amount = str(account.get_balance().get(coin_name))[:8]
+            amount = f"{account.get_balance().get(coin_name):.10f}"[
+                : self.get_accuracy()
+            ]
+            logger.info(f"{amount=}")
             self.client.place_order(
                 category="spot",
                 symbol=self.symbol,
@@ -69,7 +88,11 @@ class Orders(Client):
             return True
         except Exception as e:
             logger.error(e)
-            raise OrderPlaceExeption(f"Message: {e}")
+            raise OrderPlaceException(f"Message: {e}")
+
+    def get_avg_order(self) -> int:
+        orders = self.journal.get()["orders"]
+        return (sum(orders) / len(orders)) if len(orders) > 0 else 0
 
 
 get_orders = Orders()
