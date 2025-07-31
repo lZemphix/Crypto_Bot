2a983c8283c8fa0d508e595328c13aed2bfeb547
Zemphix <vafinaaa51@gmail.com>
2025-07-31 11:32:02 +0500

    added main file for web service

---
+++ web/main.py
@@ -0,0 +1,29 @@
+import os
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from api.index.views import router as index_router
+from api.settings.views import router as settings_router
+from api.info.views import router as info_router
+from api.statistic.views import router as statistic_router
+
+import uvicorn
+
+app = FastAPI()
+
+app.include_router(index_router)
+app.include_router(settings_router)
+app.include_router(info_router)
+app.include_router(statistic_router)
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+static_dir = os.path.join(current_dir, "static")
+
+# Mount static files
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True, port=10605, host="0.0.0.0")
+
