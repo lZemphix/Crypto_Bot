d1048d3e4c12b749327d9c70ee8349fd5cc57859
Zemphix <vafinaaa51@gmail.com>
2025-07-31 11:32:30 +0500

    added new "index" page

---
+++ web/api/index/views.py
@@ -0,0 +1,23 @@
+import json
+from fastapi import APIRouter, Depends, Request
+from fastapi.templating import Jinja2Templates
+from utils.balance import get_balance
+
+from utils.auth import verify
+
+templates = Jinja2Templates(directory="web/templates")
+router = APIRouter()
+
+
+@router.get("/", dependencies=[Depends(verify)])
+async def index(request: Request):
+    with open('bot_config.json') as f:
+        conf = json.load(f)
+    coin_name = conf['symbol'].replace('USDT', '')
+    balance = get_balance()
+    context = {"status": 'on', 
+               "pair": conf['symbol'], 
+               "balanceq": balance['USDT'], 
+               "balanceb": balance[coin_name], 
+               "interval": conf['interval']}
+    return templates.TemplateResponse(request=request, name='index.html', context=context)
