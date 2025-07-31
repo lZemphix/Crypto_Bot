594c8f9de51f2829c989d6b141d480a2fbbef85c
Zemphix <vafinaaa51@gmail.com>
2025-07-31 11:33:45 +0500

    added info page

---
+++ web/api/info/views.py
@@ -0,0 +1,12 @@
+
+from fastapi import APIRouter, Request
+
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="web/templates")
+
+router = APIRouter(prefix="/info")
+
+@router.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name='info.html')
