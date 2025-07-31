68623f10f4ddbf7c4c659a2c29fbcad0bfb467bc
Zemphix <vafinaaa51@gmail.com>
2025-07-31 11:34:41 +0500

    added statistic page, but now it's on logout role

---
+++ web/api/statistic/views.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Form, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv, set_key
+
+load_dotenv()
+
+templates = Jinja2Templates(directory="web/templates")
+
+router = APIRouter(prefix='/statistic')
+
+@router.get("/")
+async def settings(request: Request):
+    raise HTTPException(
+        status_code=401,
+    )
+    # return templates.TemplateResponse(request=request, name='statistic.html')
