a7fdde1a7a3e1387c1d4d0f94d01f72919ad83a4
Zemphix <vafinaaa51@gmail.com>
2025-08-02 22:58:36 +0500

    added type hints

--- src/utils/journal_manager.py
+++ src/utils/journal_manager.py
@@ -6,16 +6,17 @@ logger = getLogger(__name__)
 
 class JournalManager:
 
-    def get(self):
+    def get(self) -> dict:
         with open(f"src/data/trade_journal.json") as f:
             journal = json.load(f)
             return journal
 
-    def update(self, data: dict):
+    def update(self, data: dict) -> bool:
         with open("src/data/trade_journal.json", "w") as journal:
             json.dump(data, journal, indent=4)
+        return True
 
-    def clear(self):
+    def clear(self) -> bool:
         data = dict(laps=0, orders=[], buy_lines=[], sell_lines=[])
         self.update(data)
         return True
