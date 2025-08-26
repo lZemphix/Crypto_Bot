5b2a003201f518320a50fb0d42ffe43726421c67
Zemphix <vafinaaa51@gmail.com>
2025-08-26 11:44:18 +0500

    added enter and out loggers

--- src/scripts/bot.py
+++ src/scripts/bot.py
@@ -17,8 +17,10 @@ logger = getLogger(__name__)
 
 def initial_update():
     try:
+        logger.debug('enter initial_update')
         if gatekeeper_storage.update_balance():
             if gatekeeper_storage.update_klines():
+                logger.debug('out initial_update')
                 return True
     except Exception as e:
         logger.error(e)
@@ -29,9 +31,11 @@ class Price(BotBase):
         super().__init__()
 
     def get_price_side(self):
+        logger.debug('enter Price.get_price_side')
         avg_order = get_orders.get_avg_order()
         try:
             price = float(gatekeeper_storage.get_klines()[-1][4])
+            logger.debug('out Price.get_price_side')
             return round(price - avg_order, 3)
         except TypeError:
             logger.warning("TypeError. Return 0")
@@ -43,6 +47,7 @@ class Notifier(BotBase):
         super().__init__()
 
     def send_activate_notify(self):
+        logger.debug('enter Notifier.send_activate_notify')
         usdt_balance = round(gatekeeper_storage.get_balance()["USDT"], 3)
         interval = self.interval
         amount_buy = self.amount_buy
@@ -60,8 +65,11 @@ class Notifier(BotBase):
                 amount_buy=amount_buy,
             )
         )
+        logger.debug('out Notifier.send_activate_notify')
+
 
     def send_nem_notify(self):
+        logger.debug('enter Notifier.send_nem_notify')
         usdt_balance = round(gatekeeper_storage.get_balance()["USDT"], 3)
         self.telenotify.warning(
             f"Not enough money!```\nBalance: {usdt_balance}\nMin order price: {self.amount_buy}```"
@@ -69,6 +77,8 @@ class Notifier(BotBase):
         logger.warning(
             f"Not enough money! Balance: {usdt_balance}. Min order price: {self.amount_buy}"
         )
+        logger.debug('out Notifier.send_nem_notify')
+
 
 
 class States(BotBase):
@@ -81,51 +91,58 @@ class States(BotBase):
         self.sell = Sell()
 
     def insufficient_balance_state(self):
+        logger.debug('enter States.insufficient_balance_state')
         Notifier().send_nem_notify()
         logger.info("Not enough money for buying, trying to sell")
         while self.balance_trigger.invalid_balance():
             time.sleep(2)
             if self.sell.activate():
                 logger.debug("Switching to FIRST_BUY state")
+                logger.debug('out States.insufficient_balance_state')
                 return BotState.FIRST_BUY
 
     def waiting_state(self):
-        logger.debug("State: WAITING")
+        logger.debug('enter States.waiting_state')
         if len(self.journal.get()["orders"]) == 0:
-            logger.debug("No orders. Switching to FIRST_BUY state")
+            logger.debug('out States.waiting_state FirstBuy')
             return BotState.FIRST_BUY
         elif Price().get_price_side() > 0:
-            logger.debug("Coin price > 0. Switching to SELL state")
+            logger.debug('out States.waiting_state Sell')
             return BotState.SELL
         else:
-            logger.debug("Coin price < 0. Switching to AVERAGING state")
+            logger.debug('out States.waiting_state Avg')
             return BotState.AVERAGING
 
     def averaging_state(self):
+        logger.debug('enter States.averaging_state')
         if self.averaging.activate():
-            logger.info("Averaged. Switching to WAITING state")
             gatekeeper_storage.update_balance()
             MetaManager().update_all(type="average")
-        else:
-            logger.info("Not averaged. Switching to WAITING state")
+            logger.info("Averaged. Switching to WAITING state")
+        logger.debug('out States.waiting_state')
         return BotState.WAITING
 
     def sell_state(self):
+        logger.debug('enter States.sell_state')
         if self.sell.activate():
             logger.info("Sold. Switching to FIRST_BUY state")
             gatekeeper_storage.update_balance()
             MetaManager().update_all(type="sell")
+            logger.debug('out States.sell_state First')
             return BotState.FIRST_BUY
         else:
-            logger.info("Not sold. Switching to WAITING state")
+            logger.debug('out States.sell_state Wait')
             return BotState.WAITING
 
     def first_buy_state(self):
+        logger.debug('enter States.first_buy_state')
         if self.first_buy.activate():
             logger.info("Bought. Switching to WAITING state")
             gatekeeper_storage.update_balance()
             MetaManager().update_all(type="first buy")
+            logger.debug('out States.first_buy_state WAITING' )
             return BotState.WAITING
+        logger.debug('out States.first_buy_state FIRST_BUY')
         return BotState.FIRST_BUY
 
 
@@ -135,15 +152,15 @@ class Bot(BotBase):
         super().__init__()
 
     def activate(self):
+        logger.debug('enter Bot.activate')
         Gatekeeper()
         if not initial_update():
             raise SystemExit("Can't do initial update")
         current_state = BotState.WAITING
-        logger.info("Bot was activated!")
         Notifier().send_activate_notify()
+        logger.info("Bot was activated!")
         while True:
             time.sleep(1)
-            logger.debug(f"Current state: {current_state}")
 
             if BalanceTrigger().invalid_balance():
                 current_state = States().insufficient_balance_state()
